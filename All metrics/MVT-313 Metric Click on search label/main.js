d0128a79951a86a2f41194556ff49965875feb53
Rebirth <qz619136084@163.com>
2022-05-26 14:33:26 +0800

    normal update

--- All metrics/MVT-313 Metric Click on search label/main.js
+++ All metrics/MVT-313 Metric Click on search label/main.js
@@ -13,4 +13,18 @@ $(function () {
     100,
     10
   );
+  DYO.waitForElement(
+    ".dy-filter-outer a",
+    function () {
+      $(".dy-filter-outer a").click(function () {
+        DY.API("event", {
+          name: "MVT-313 Metric: Click on restaurants label",
+          properties: {},
+        });
+      });
+    },
+    1,
+    100,
+    30
+  );
 });
