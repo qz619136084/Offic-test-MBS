262bc235514bb162ff6803d73a983b1d0657ae47
Rebirth <qz619136084@163.com>
2022-05-26 17:35:48 +0800

    313 & 346

--- All metrics/MVT-313 Metric Click on search label/main.js
+++ All metrics/MVT-313 Metric Click on search label/main.js
@@ -1,24 +1,10 @@
 $(function () {
   DYO.waitForElement(
-    ".search-filters",
-    function () {
-      $(".search-filters").click(function () {
-        DY.API("event", {
-          name: "MVT-313 Metric: Click on search label",
-          properties: {},
-        });
-      });
-    },
-    1,
-    100,
-    10
-  );
-  DYO.waitForElement(
     ".dy-filter-outer a",
     function () {
       $(".dy-filter-outer a").click(function () {
         DY.API("event", {
-          name: "MVT-313 Metric: Click on restaurants label",
+          name: "MVT-313 Metric: Click on search label",
           properties: {},
         });
       });
