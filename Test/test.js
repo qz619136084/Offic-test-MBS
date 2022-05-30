da26907aa09f15e99c4afbef4b94ed23fb001690
Rebirth <qz619136084@163.com>
2022-05-30 17:59:37 +0800

    update request testing for localmeasure

--- Test/test.js
+++ Test/test.js
@@ -1,30 +1,19 @@
-$(function () {
-  DYO.waitForElement(
-    ".btn.btn-primary[data-toggle=bookroom]",
-    function () {
-      $(".btn.btn-primary[data-toggle=bookroom]").click(() => {
-        DY.API("event", {
-          name: "Click on BOOK CTA (navBar + page) on hotel deals pages",
-          properties: {},
-        });
-      });
-    },
-    1,
-    100,
-    10
-  );
-  DYO.waitForElement(
-    "a.btn.btn-primary",
-    function () {
-      $("a.btn.btn-primary:not(.dy-srl-component a)").click(function () {
-        DY.API("event", {
-          name: "Click on BOOK CTA (navBar + page) on hotel deals pages",
-          properties: {},
-        });
-      });
-    },
-    1,
-    100,
-    10
-  );
-});
+function ajaxRequest() {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      type: "GET",
+      url: "https://cdn.getlocalmeasure.com/public/2c487b798290e72336606ff881cb35d404777a2ca0ea5911014ca07d20e0/data.js",
+      dataType: "jsonp",
+      jsonpCallback:
+        "jsonCallback_2c487b798290e72336606ff881cb35d404777a2ca0ea5911014ca07d20e0",
+      success: function (res) {
+        resolve(res);
+      },
+    });
+  });
+}
+async function getDate() {
+  var a = await ajaxRequest();
+  console.log(a);
+}
+getDate();
