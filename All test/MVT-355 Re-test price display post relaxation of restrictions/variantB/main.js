6ea41b4e6802c43690b7f72a15ebfa2a5397c1ef
Rebirth <qz619136084@163.com>
2022-06-08 11:36:15 +0800

    update mvt-355

--- All test/MVT-355 Re-test price display post relaxation of restrictions/variantB/main.js
+++ All test/MVT-355 Re-test price display post relaxation of restrictions/variantB/main.js
@@ -5,12 +5,12 @@ $(function () {
     var textTarget = ".dy-new-layout .big~br+small";
     var priceTarget = ".dy-new-layout .big strong";
     handleElSync(textTarget, priceTarget, updatedText);
-    $(".dropdown-menu-filter .checkbox input").click(function () {
+    $(".dropdown-menu-filter .checkbox input").click(() => {
       setTimeout(() => {
         handleElSync(textTarget, priceTarget, updatedText);
       }, 10);
     });
-    $(".dropdown-menu-filter .filter-clear").click(function () {
+    $(".dropdown-menu-filter .filter-clear").click(() => {
       setTimeout(() => {
         handleElSync(textTarget, priceTarget, updatedText);
       }, 10);
@@ -20,25 +20,28 @@ $(function () {
     var priceTarget = ".big strong";
     handleElSync(textTarget, priceTarget, updatedText);
   } else if (url.indexOf("booking/rooms") > -1) {
-    var waitComponentShow = setInterval(function () {
+    var waitComponentShow = setInterval(() => {
       var stepSelected = $(".inner_circle_step.shapeborder_selected_in");
       if (stepSelected.length == 2) {
-        var getData = setInterval(function () {
+        var getData = setInterval(() => {
           if (
             f_getSessionStorage() != undefined &&
             $("#loading").css("display") == "none"
           ) {
+            updateInRoomList();
             bindMainClick();
-            var bindEditClick1717 = setInterval(function () {
+            bindFilterClick();
+            observe_multiRoomBanner();
+            var bindEditClick1717 = setInterval(() => {
               if ($("#booking_information_edit").length) {
-                $("#booking_information_edit").click(function () {
+                $("#booking_information_edit").click(() => {
                   var currentDate = f_getSessionStorage().checkindate;
                   var currentLos = f_getSessionStorage().los;
                   var currentRooms = f_getSessionStorage().rooms;
-                  var bindSelectClick = setInterval(function () {
+                  var bindSelectClick = setInterval(() => {
                     if ($("#s_btn_view_rooms").length) {
-                      $("#s_btn_view_rooms").click(function () {
-                        var waitUpdatedSession = setInterval(function () {
+                      $("#s_btn_view_rooms").click(() => {
+                        var waitUpdatedSession = setInterval(() => {
                           if (
                             f_getSessionStorage() != undefined &&
                             $("#loading").css("display") == "none"
@@ -58,7 +61,11 @@ $(function () {
                                 currentRooms == calendarRooms
                               )
                             ) {
-                              bindClickToAddText();
+                              updateInRoomList();
+                              updateInSingleRoomCard();
+                              bindMainClick();
+                              bindFilterClick();
+                              observe_multiRoomBanner();
                             }
                             clearInterval(waitUpdatedSession);
                           }
@@ -77,14 +84,11 @@ $(function () {
         clearInterval(waitComponentShow);
       }
     }, 100);
+  } else if (url.indexOf("booking/multirooms") > -1) {
+    updateInMultiRoomCard();
   }
 
-  async function handleElSync(
-    textTarget,
-    priceTarget,
-    updatedText,
-    be = false
-  ) {
+  async function handleElSync(textTarget, priceTarget, updatedText, be) {
     //text target;
     var targetEl = await DYO.waitForElementAsync(textTarget);
     $(targetEl).text(updatedText);
@@ -95,58 +99,185 @@ $(function () {
       var priceWithoutTax = null;
       var _this = $(this);
       if (!be) {
+        //For Non-BE
         //"+" is in order to change String to Number;
         currencySymbol = _this.text().slice(0, 2);
         priceWithoutTax = +_this.text().slice(2).replace(/,/g, "");
         priceWithTax = toMoney(priceWithoutTax * 1.177);
       } else {
         //For BE
-        var packageTitle = _this
-          .closest(".packageList_item")
-          .find(".packageList_item_title")
-          .text()
-          .trim();
-        console.log("packageTitle:", packageTitle);
-        var currency = f_getCurrencyInfo().code;
-        var discountedAveragePriceByCurrency =
-          curr_availableRateplans.offerList.filter((item) => {
-            return item.name === packageTitle;
-          })[0].rooms[0].discountedAveragePriceByCurrency;
-        currencySymbol = f_getCurrencyInfo().symbol;
-        if (discountedAveragePriceByCurrency.length) {
-          console.log("has discount");
-          //set up for discounted price
-          priceWithoutTax = discountedAveragePriceByCurrency.filter((item) => {
-            return item.currencyCode === currency;
-          })[0].price;
-          console.log("price:", priceWithoutTax);
-          priceWithTax = toMoney(priceWithoutTax * 1.177, true);
-          //set up for origin price;
-          var originPriceTarget = _this
-            .closest(".packageList_item_pricing")
-            .find(".original_price");
-          var originPriceWithoutTax = curr_availableRateplans.offerList
-            .filter((item) => {
+        if ($("#select_room_container").length) {
+          //in single room card
+          var packageTitle = _this
+            .closest(".packageList_item")
+            .find(".packageList_item_title")
+            .text()
+            .trim();
+          var currency = f_getCurrencyInfo().code;
+          var discountedAveragePriceByCurrency =
+            curr_availableRateplans.offerList.filter((item) => {
               return item.name === packageTitle;
-            })[0]
-            .rooms[0].averagePriceByCurrency.filter((item) => {
-              return item.currencyCode === currency;
-            })[0].price;
-          console.log("originPrice:", originPriceWithoutTax);
-          var originPriceWithTax = toMoney(originPriceWithoutTax * 1.177, true);
-          originPriceTarget.text(currencySymbol + originPriceWithTax);
+            })[0].rooms[0].discountedAveragePriceByCurrency;
+          currencySymbol = f_getCurrencyInfo().symbol;
+          if (discountedAveragePriceByCurrency.length) {
+            //have discount
+            priceWithoutTax = discountedAveragePriceByCurrency.filter(
+              (item) => {
+                return item.currencyCode === currency;
+              }
+            )[0].price;
+            priceWithTax = toMoney(priceWithoutTax * 1.177, true);
+            //set up for origin price;
+            var originPriceTarget = _this
+              .closest(".packageList_item_pricing")
+              .find(".original_price");
+            var originPriceWithoutTax = curr_availableRateplans.offerList
+              .filter((item) => {
+                return item.name === packageTitle;
+              })[0]
+              .rooms[0].averagePriceByCurrency.filter((item) => {
+                return item.currencyCode === currency;
+              })[0].price;
+            var originPriceWithTax = toMoney(
+              originPriceWithoutTax * 1.177,
+              true
+            );
+            originPriceTarget.text(currencySymbol + originPriceWithTax);
+          } else {
+            //no discount
+            priceWithoutTax = curr_availableRateplans.offerList
+              .filter((item) => {
+                return item.name === packageTitle;
+              })[0]
+              .rooms[0].averagePriceByCurrency.filter((item) => {
+                return item.currencyCode === currency;
+              })[0].price;
+            priceWithTax = toMoney(priceWithoutTax * 1.177, true);
+          }
+        } else if ($("#multiroom_list").length) {
+          //in multiroom card
+          //record price;
+          if (_this.is($(".rate_cal_counter .multi_number"))) {
+            var totalPrice = 0;
+            _this
+              .closest(".room_card")
+              .find(".multi_number[price-data]")
+              .each(function () {
+                totalPrice += +$(this).attr("price-data");
+              });
+            currencySymbol = f_getCurrencyInfo().symbol;
+            priceWithTax = toMoney(totalPrice * 1.177, true);
+          } else {
+            var packageTitle = _this
+              .closest(".room_card.card")
+              .find(".card_title")
+              .text()
+              .trim();
+            var roomType = _this
+              .closest(".row")
+              .find(".col-md-5 strong")
+              .text()
+              .trim();
+            var currency = f_getCurrencyInfo().code;
+            var discountedAveragePriceByCurrency = curr_availableRateplans
+              .filter((item) => {
+                return item.name === packageTitle;
+              })[0]
+              .rooms.filter((item) => {
+                return item.roomName === roomType;
+              })[0].discountedAveragePriceByCurrency;
+            currencySymbol = f_getCurrencyInfo().symbol;
+            if (discountedAveragePriceByCurrency.length) {
+              //have discount
+              priceWithoutTax = discountedAveragePriceByCurrency.filter(
+                (item) => {
+                  return item.currencyCode === currency;
+                }
+              )[0].price;
+              priceWithTax = toMoney(priceWithoutTax * 1.177, true);
+              //set up for origin price;
+              var originPriceTarget = _this.closest(".row").find(".del_txt");
+              var originPriceWithoutTax = curr_availableRateplans
+                .filter((item) => {
+                  return item.name === packageTitle;
+                })[0]
+                .rooms.filter((item) => {
+                  return item.roomName === roomType;
+                })[0]
+                .averagePriceByCurrency.filter((item) => {
+                  return item.currencyCode === currency;
+                })[0].price;
+              var originPriceWithTax = toMoney(
+                originPriceWithoutTax * 1.177,
+                true
+              );
+              originPriceTarget.text(currencySymbol + originPriceWithTax);
+            } else {
+              //no discount
+              priceWithoutTax = curr_availableRateplans
+                .filter((item) => {
+                  return item.name === packageTitle;
+                })[0]
+                .rooms.filter((item) => {
+                  return item.roomName === roomType;
+                })[0]
+                .averagePriceByCurrency.filter((item) => {
+                  return item.currencyCode === currency;
+                })[0].price;
+              priceWithTax = toMoney(priceWithoutTax * 1.177, true);
+            }
+            _this.attr("price-data", priceWithoutTax);
+          }
         } else {
-          console.log("no discount");
-          //set up for discounted price
-          priceWithoutTax = curr_availableRateplans.offerList
-            .filter((item) => {
-              return item.name === packageTitle;
-            })[0]
-            .rooms[0].averagePriceByCurrency.filter((item) => {
-              return item.currencyCode === currency;
-            })[0].price;
-          console.log("price:", priceWithoutTax);
-          priceWithTax = toMoney(priceWithoutTax * 1.177, true);
+          //in Room List
+          var roomTitle = _this
+            .closest(".room_card.card")
+            .find(".txt-black-five")
+            .text()
+            .replace(" - ", "")
+            .trim();
+          var currency = f_getCurrencyInfo().code;
+          //if "discountedAveragePriceByCurrency" is an array with no value, then it means that no discount and "price" will be placed in "avaragePriceByCurrency"
+          var discountedAveragePriceByCurrency =
+            f_getSessionStorage().availableRooms.filter((item) => {
+              return item.name === roomTitle;
+            })[0].discountedAveragePriceByCurrency;
+          currencySymbol = f_getCurrencyInfo().symbol;
+          if (discountedAveragePriceByCurrency.length) {
+            //have discount
+            priceWithoutTax = discountedAveragePriceByCurrency.filter(
+              (item) => {
+                return item.currencyCode === currency;
+              }
+            )[0].price;
+            priceWithTax = toMoney(priceWithoutTax * 1.177, true);
+            //set up for origin price;
+            var originPriceTarget = _this
+              .closest(".pricePanel")
+              .find(".original_price");
+            var originPriceWithoutTax = f_getSessionStorage()
+              .availableRooms.filter((item) => {
+                return item.name === roomTitle;
+              })[0]
+              .averagePriceByCurrency.filter((item) => {
+                return item.currencyCode === currency;
+              })[0].price;
+            var originPriceWithTax = toMoney(
+              originPriceWithoutTax * 1.177,
+              true
+            );
+            originPriceTarget.text(currencySymbol + originPriceWithTax);
+          } else {
+            //no discount
+            priceWithoutTax = f_getSessionStorage()
+              .availableRooms.filter((item) => {
+                return item.name === packageTitle;
+              })[0]
+              .averagePriceByCurrency.filter((item) => {
+                return item.currencyCode === currency;
+              })[0].price;
+            priceWithTax = toMoney(priceWithoutTax * 1.177, true);
+          }
         }
       }
       _this.text(currencySymbol + priceWithTax);
@@ -183,13 +314,123 @@ $(function () {
     }
   }
   function bindMainClick() {
+    $(".mbs_button_primary").unbind("click.taxes");
+    $(".room_card .close_button").unbind("click.taxes");
     DYO.waitForElementAsync(".mbs_button_primary").then(() => {
-      $(".mbs_button_primary").click(function () {
-        console.log("click");
-        var textTarget = ".packageList_item_pricing small";
-        var priceTarget = ".packagePrice";
-        handleElSync(textTarget, priceTarget, updatedText, true);
+      $(".mbs_button_primary").bind("click.taxes", () => {
+        updateInSingleRoomCard();
       });
+    });
+    DYO.waitForElementAsync(".room_card .close_button").then(() => {
+      $(".room_card .close_button").bind("click.taxes", () => {
+        updateInRoomList();
+      });
+    });
+  }
+  function updateInRoomList() {
+    //add class name Mark so that DY api could select easier, cause DY api use JS so that it can't use complex selector
+    $(".d-sm-block:has(.room_price)+div small").addClass("tax-tip");
+    var textTarget = ".tax-tip";
+    var priceTarget = ".room_price";
+    handleElSync(textTarget, priceTarget, updatedText, true);
+  }
+  function updateInSingleRoomCard() {
+    delayUpdate().then(() => {
+      var textTarget = ".packageList_item_pricing small";
+      var priceTarget = ".packagePrice";
+      handleElSync(textTarget, priceTarget, updatedText, true);
+      if (!$(".radio_div.binded").length) {
+        bindBedtypeRadioClick();
+      }
+    });
+  }
+  function bindBedtypeRadioClick() {
+    $(".radio_div").unbind("click.taxes").removeClass("binded");
+    DYO.waitForElementAsync(".radio_div").then(() => {
+      $(".radio_div").bind("click.taxes", () => {
+        updateInSingleRoomCard();
+      });
+      $(".radio_div").addClass("binded");
+    });
+  }
+  function updateInMultiRoomCard() {
+    var textTarget = ".room_card .multi_avg~div small";
+    var priceTarget = ".multi_number";
+    handleElSync(textTarget, priceTarget, updatedText, true);
+  }
+  function bindFilterClick() {
+    $(".room_filters_dropdown:not(.dy-bath-filter) a+ul li").unbind(
+      "click.taxes"
+    );
+    $(".room_filters_dropdown.dy-bath-filter a+ul li input").unbind(
+      "click.taxes"
+    );
+    $(".currency_dropdown a+ul li").unbind("click.taxes");
+    $("#sort_by").unbind("click.taxes");
+    $(".room_filters_dropdown:not(.dy-bath-filter) a+ul li").bind(
+      "click.taxes",
+      () => {
+        updateInRoomList();
+      }
+    );
+    $(".room_filters_dropdown.dy-bath-filter a+ul li input").bind(
+      "click.taxes",
+      () => {
+        updateInRoomList();
+      }
+    );
+    $(".currency_dropdown a+ul li").bind("click.taxes", () => {
+      updateInRoomList();
+    });
+    $("#sort_by").bind("click.taxes", () => {
+      updateInRoomList();
+    });
+  }
+  function observe_multiRoomBanner() {
+    var roomsNo = getParaQuery("Rooms");
+    if (roomsNo > 1) {
+      var targetNode = document.getElementById("multiRoomBanner_outlet");
+      var config = { attributes: true, childList: true, subtree: true };
+      var callback = function (mutationsList, observer) {
+        for (let mutation of mutationsList) {
+          if (mutation.type === "childList") {
+            var addedNodes = mutation.addedNodes;
+            var removedNodes = mutation.removedNodes;
+            if (
+              $.inArray($(".banner_card_outlet")[0], addedNodes) > -1 ||
+              $.inArray($(".banner_card_outlet")[0], removedNodes) > -1
+            ) {
+              setTimeout(() => {
+                updateInRoomList();
+                bindFilterClick();
+                bindMainClick();
+              }, 300);
+            }
+          }
+        }
+      };
+      var observer = new MutationObserver(callback);
+      observer.observe(targetNode, config);
+    }
+  }
+  function getParaQuery(para) {
+    var paraString = window.location.search.substring(1);
+    var paraArr = paraString.split("&");
+    for (i = 0; i < paraArr.length; i++) {
+      var name = paraArr[i].split("=")[0];
+      var value = paraArr[i].split("=")[1];
+      if (para == name) {
+        return value;
+      }
+    }
+  }
+  function delayUpdate() {
+    return new Promise((resolve, reject) => {
+      setInterval(() => {
+        if ($("#loading").css("display") == "none") {
+          resolve();
+        }
+      }, 100);
     });
   }
 });
--- Test/test.js
+++ Test/test.js
@@ -1,17 +1,48 @@
-$(function () {
-  DYO.waitForElementAsync(".inner_circle_step.shapeborder_selected_in", 2)
-    .then(() => {
-      return new Promise((resolve, reject) => {
-        if (
-          f_getSessionStorage() != undefined &&
-          $("#loading").css("display") == "none"
-        ) {
-          resolve();
-        }
-      });
-    })
-    .then(() => {
-      onDataReady();
-    });
-  function onDataReady() {}
-});
+
+          //in single Room Card
+          var packageTitle = _this
+            .closest(".packageList_item")
+            .find(".packageList_item_title")
+            .text()
+            .trim();
+          var currency = f_getCurrencyInfo().code;
+          var discountedAveragePriceByCurrency =
+            curr_availableRateplans.offerList.filter((item) => {
+              return item.name === packageTitle;
+            })[0].rooms[0].discountedAveragePriceByCurrency;
+          currencySymbol = f_getCurrencyInfo().symbol;
+          if (discountedAveragePriceByCurrency.length) {
+            //have discount
+            priceWithoutTax = discountedAveragePriceByCurrency.filter(
+              (item) => {
+                return item.currencyCode === currency;
+              }
+            )[0].price;
+            priceWithTax = toMoney(priceWithoutTax * 1.177, true);
+            //set up for origin price;
+            var originPriceTarget = _this
+              .closest(".packageList_item_pricing")
+              .find(".original_price");
+            var originPriceWithoutTax = curr_availableRateplans.offerList
+              .filter((item) => {
+                return item.name === packageTitle;
+              })[0]
+              .rooms[0].averagePriceByCurrency.filter((item) => {
+                return item.currencyCode === currency;
+              })[0].price;
+            var originPriceWithTax = toMoney(
+              originPriceWithoutTax * 1.177,
+              true
+            );
+            originPriceTarget.text(currencySymbol + originPriceWithTax);
+          } else {
+            //no discount
+            priceWithoutTax = curr_availableRateplans.offerList
+              .filter((item) => {
+                return item.name === packageTitle;
+              })[0]
+              .rooms[0].averagePriceByCurrency.filter((item) => {
+                return item.currencyCode === currency;
+              })[0].price;
+            priceWithTax = toMoney(priceWithoutTax * 1.177, true);
+          }
