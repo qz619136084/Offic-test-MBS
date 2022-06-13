2c20b6cbb686235291c13bef8c7cd08f04a2b7ee
Rebirth <qz619136084@163.com>
2022-06-13 09:19:03 +0800

    normal update

--- Test/test.js
+++ Test/test.js
@@ -1,4 +1,1 @@
-function test() {
-  console.log("scrolling");
-}
-window.onscroll = test;
+$(function(){$(".signupForm").before('<div class="dy-srl-component"> <h3 class="title">Exclusive Member Benefits</h3> <div class="content"> <div class="content-detail"> <p> <img src="/content/dam/singapore/marinabaysands/master/main/images/DY-MVT-346/icon-hand-with-dollar.svg" alt="icon-hand-with-dollar" />Earn up to 10% Reward Dollars and redeem at over 250 outlets </p> <p> <img src="/content/dam/singapore/marinabaysands/master/main/images/DY-MVT-346/icon-dollars.svg" alt="icon-dollars" />Enjoy savings on attractions and museum tickets </p> </div> <div class="content-detail"> <p> <img src="/content/dam/singapore/marinabaysands/master/main/images/DY-MVT-346/icon-car.svg" alt="icon-car" />All-day parking benefits</p></div> </div> </div>')});
