cd1b7224cd2a45757bd01454c44bc6a02891d8b6
Rebirth <qz619136084@163.com>
2022-06-14 16:50:37 +0800

    normal update

--- All test/Mobile App - Smart app banner (Loyalty site)/non-safari.js
+++ All test/Mobile App - Smart app banner (Loyalty site)/non-safari.js
@@ -9,9 +9,15 @@ $(function () {
       ua.indexOf("WALSHHOTEL") == -1 &&
       ua.indexOf("INTELITY") == -1
     ) {
-      $(".navRow").before(
-        "<div class='dy-smart-app-banner'> <div class='close-button'></div> <div class='img-wrap'> <img src='/content/dam/singapore/marinabaysands/mbs/asm/logo-mini.svg' alt='' /> </div> <div class='content-wrap'> <p class='title'>Marina Bay Sands Singapore</p> <p class='sub-title'>Find your way quickly, redeem free parking and more</p> </div> <a class='download-button' href='https://www.marinabaysands.com/appdl/' >View</a > </div>"
-      );
+      if ($("#wtBody .navRow").length) {
+        $("#wtBody .navRow").before(
+          "<div class='dy-smart-app-banner'> <div class='close-button'></div> <div class='img-wrap'> <img src='/content/dam/singapore/marinabaysands/mbs/asm/logo-mini.svg' alt='' /> </div> <div class='content-wrap'> <p class='title'>Marina Bay Sands Singapore</p> <p class='sub-title'>Find your way quickly, redeem free parking and more</p> </div> <a class='download-button' href='https://www.marinabaysands.com/appdl/' >View</a > </div>"
+        );
+      } else {
+        $(".col:has(.navWrapper)").before(
+          "<div class='dy-smart-app-banner'> <div class='close-button'></div> <div class='img-wrap'> <img src='/content/dam/singapore/marinabaysands/mbs/asm/logo-mini.svg' alt='' /> </div> <div class='content-wrap'> <p class='title'>Marina Bay Sands Singapore</p> <p class='sub-title'>Find your way quickly, redeem free parking and more</p> </div> <a class='download-button' href='https://www.marinabaysands.com/appdl/' >View</a > </div>"
+        );
+      }
       $("head").append(
         "<style>.dy-smart-app-banner .download-button { text-decoration: none; } .dy-smart-app-banner .content-wrap .title { margin: 0; } .dy-smart-app-banner .content-wrap .sub-title { margin: 0; } @media (max-width: 376px) { .dy-smart-app-banner .content-wrap .title { font-size: 0.7rem; } .dy-smart-app-banner .content-wrap .sub-title { font-size: 0.6rem; } }</style>"
       );
