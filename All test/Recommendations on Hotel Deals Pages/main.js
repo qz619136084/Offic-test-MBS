d97156636e41e7cc09e1b4727872553ba4c45962
Rebirth <qz619136084@163.com>
2022-06-23 17:12:25 +0800

    update hotel recommendation widget bug fix

--- All test/Recommendations on Hotel Deals Pages/main.js
+++ All test/Recommendations on Hotel Deals Pages/main.js
@@ -6,6 +6,8 @@ DYO.waitForElementAsync(
     setRedirectFunctionality(tile);
     setHoverFunctionality(tile);
   });
+  //Bryant Added;
+  bindFilterClick();
 });
 
 function setRedirectFunctionality(node) {
@@ -34,4 +36,53 @@ function setHoverFunctionality(node) {
       }, 150);
     });
   });
+}
+function bindFilterClick() {
+  $(".dropdown-menu-filter .checkbox input").click(function () {
+    var selectedArr = [];
+    $(".dropdown-menu-filter label:has(input:checked)").each(function () {
+      var selectedPerk = $(this).text().trim();
+      selectedArr.push(selectedPerk);
+    });
+    console.log("selectedArr:", selectedArr);
+    adjustLayout(selectedArr);
+  });
+  $(".dropdown-menu-filter .filter-clear").click(function () {
+    $(".dy-new-layout").show();
+  });
+}
+function adjustLayout(arr) {
+  if (arr.length) {
+    $(".dy-new-layout").each(function () {
+      var _this = $(this);
+      var perksArr = [];
+      var canShow = 1;
+      _this.find(".icon-content").each(function () {
+        var perk = $(this)
+          .find(".tip-content")
+          .text()
+          .trim()
+          .replaceAll("*", "");
+        if ($(this).css("display") != "none") {
+          perksArr.push(perk);
+        }
+      });
+      //compare info with filter for each tile;
+      console.log(perksArr);
+      arr.forEach((v) => {
+        if ($.inArray(v, perksArr) == -1) {
+          canShow = 0;
+          console.log("can't show");
+        }
+      });
+      //control show/hide for each tile with filter
+      if (canShow) {
+        _this.show();
+      } else {
+        _this.hide();
+      }
+    });
+  } else {
+    $(".dy-new-layout").show();
+  }
 }
