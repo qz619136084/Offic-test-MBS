2cef949587c208dc479580b34860e2b258886229
RebirthQ <qz619136084@163.com>
2022-07-28 10:13:32 +0800

    update

--- Test/test1.js
+++ Test/test1.js
@@ -1,16 +1,3 @@
-new DYO.Q.Promise((resolve) => {
-  DY.API("callback", () => {
-    if (DY.itemsData[0]) {
-      var categoryArr = DY.itemsData[0].categories;
-      var targetArr = ["cskypark2018", "dlc2020", "sampan2019"];
-      targetArr.forEach((v) => {
-        if (jQuery.inArray(v, categoryArr) > -1) {
-          resolve(v);
-        }
-      });
-    }
-  });
-}).then((res) => {
-  console.log(res);
-  return res;
-});
+setInterval(() => {
+  console.log("1");
+}, 1000);
